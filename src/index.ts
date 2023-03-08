b6d5b4a7fe06c85eb93fd5e0e04ef1a7adc0d5d3
Adrian Rall <adrian@asda4.de>
2023-03-08 12:09:21 +0100

    sources includes

--- src/index.ts
+++ src/index.ts
@@ -578,7 +578,7 @@ async function getAllNuGetPackages(projectList: string[], sourceList: string[]):
 
   allPackages.forEach((packageInfo) => {
     //const isInternal = packageInfo.project === './Blazor4/BlazorApp4/BlazorApp4/BlazorApp4.csproj';
-    const isInternal = s.has(packageInfo.project);
+    const isInternal = sources.includes(packageInfo.project)
     if (isInternal) {
       internPackages.push(packageInfo);
     } else {
