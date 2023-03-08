d57c5c1f6f9e3dc6208416d02b8690917584dea2
Adrian Rall <adrian@asda4.de>
2023-03-08 13:15:56 +0100

    isInternal = sources.search

--- src/index.ts
+++ src/index.ts
@@ -579,16 +579,13 @@ async function getAllNuGetPackages(projectList: string[], sourceList: string[]):
   allPackages.forEach((packageInfo) => {
     //const isInternal2 = packageInfo.project === './Blazor4/BlazorApp4/BlazorApp4/BlazorApp4.csproj'
     //const isInternal2 = sources[0].search(packageInfo.project); //=== './Blazor4/BlazorApp4/BlazorApp4/BlazorApp4.csproj'
-    let isInternal2 = false;
-    if(sources[0].search(packageInfo.project)) {
-    isInternal2 = true;
-    } 
+    let isInternal2 = sources[0].search(packageInfo.project);
+
     console.log(`isInternal2 has value ${isInternal2}`);
     console.log(`sources has value ${sources[0]} and packageInfo is ${ packageInfo.project}`);
     //const isInternal = sources.includes(packageInfo.project);
     if (isInternal2) {
       internPackages.push(packageInfo);
-      isInternal2 = false
     } else {
       externPackages.push(packageInfo);
     }
