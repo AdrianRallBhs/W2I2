f00d4eea0e0d01062d12d5272207d515fa3a2f98
Adrian Rall <adrian@asda4.de>
2023-03-09 21:25:58 +0100

    sdfljgprkv

---
+++ src/w2i.ts
@@ -0,0 +1,38 @@
+import { execSync } from "child_process";
+import * as fs from 'fs';
+
+const repositoryUrl = "https://github.com/AdrianRallBhs/W2I2.git";
+
+// Replace with the name of the branch you want to push to
+const featureBranchName = "feature-test";
+
+const repositoryDir = "W2I2";
+
+const email = "adrian@asda4.de"
+
+const name = "AdrianRallBhs" || "Adrian Rall";
+
+try {
+  // Clone the repository
+  execSync(`git clone ${repositoryUrl}`);
+
+  process.chdir(repositoryDir);
+  execSync(`git checkout ${featureBranchName}`, { shell: "/bin/bash" });
+
+  execSync(`git config --global user.email ${email}`);
+execSync(`git config --global user.name \"${name}\"`);
+
+  // Run the function and write the output to a file
+  const output = " Hallo "
+  fs.writeFileSync("output.json", JSON.stringify(output, null, 2));
+
+  // Ensure that the remote repository is set up correctly
+  execSync(`git remote set-url origin ${repositoryUrl}`);
+
+  // Commit and push the changes to the feature branch
+  execSync(`git add .`);
+  execSync(`git commit -m "Add output to output.json"`);
+  execSync(`git push origin ${featureBranchName}`);
+} catch (error) {
+  console.error(error);
+}
