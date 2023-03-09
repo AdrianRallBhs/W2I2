2ffecd1c9b8a44177e91eae9f7ea24e35fc7830d
Adrian Rall <adrian@asda4.de>
2023-03-09 22:29:13 +0100

    osidf

--- src/w2i.ts
+++ src/w2i.ts
@@ -1,7 +1,7 @@
 import { execSync } from "child_process";
 import * as fs from 'fs';
 
-const repositoryUrl = "https://AdrianRall:^R4fJ23;GEW(github.com/AdrianRallBhs/W2I2.githttps://github.com/AdrianRallBhs/W2I2.git";
+const repositoryUrl = "https://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@github.com/AdrianRallBhs/W2I2.githttps://github.com/AdrianRallBhs/W2I2.git";
 
 // Replace with the name of the branch you want to push to
 const featureBranchName = "feature-test";
@@ -10,7 +10,7 @@ const repositoryDir = "W2I2";
 
 const email = "adrian@asda4.de"
 
-const name = "AdrianRallBhs" || "Adrian Rall" || "AdrianRall";
+const name = "AdrianRall";
 
 try {
  // Clone the repository
@@ -19,8 +19,8 @@ try {
   process.chdir(repositoryDir);
   execSync(`git checkout ${featureBranchName}`, { shell: "/bin/bash" });
 
-//   execSync(`git config --global user.email ${email}`);
-// execSync(`git config --global user.name \"${name}\"`);
+  execSync(`git config --global user.email ${email}`);
+execSync(`git config --global user.name "${name}"`);
 
   // Run the function and write the output to a file
   const output = " Hallo "
