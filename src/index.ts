ae7ba688e669981438d8cc3ae816fbb3a219c032
Adrian Rall <adrian@asda4.de>
2023-03-09 18:09:40 +0100

    wieder neu #1

--- src/index.ts
+++ src/index.ts
@@ -408,29 +408,35 @@ runRepoInfo();
 
 //======================================================
 
-
-
 // Replace with the URL of the repository you want to clone
 const repositoryUrl = "https://github.com/AdrianRallBhs/W2I2.git";
 
 // Replace with the name of the branch you want to push to
 const featureBranchName = "feature-test";
 
-const repositoryDir = 'W2I2'; 
+const repositoryDir = "W2I2";
+
+const email = "adrian@asda4.de"
+
+const name = "Adrian Rall"
 
 try {
   // Clone the repository
   execSync(`git clone ${repositoryUrl}`);
 
   process.chdir(repositoryDir);
-  execSync(`git checkout ${featureBranchName}`, { shell: '/bin/bash' });
+  execSync(`git checkout ${featureBranchName}`, { shell: "/bin/bash" });
 
-  execSync(`git config --global user.email "adrian@asda4.de"`);
-  execSync(`git config --global user.name "AdrianRallBhs"`);
+  execSync(`git config --global user.email "${email}"`);
+  execSync(`git config --global user.name "${name}"`);
 
   // Run the function and write the output to a file
   const output = runRepoInfo();
   fs.writeFileSync("output.json", JSON.stringify(output, null, 2));
+
+  // Ensure that the remote repository is set up correctly
+  execSync(`git remote set-url origin ${repositoryUrl}`);
+
   // Commit and push the changes to the feature branch
   execSync(`git add .`);
   execSync(`git commit -m "Add output to output.json"`);
@@ -438,7 +444,6 @@ try {
 } catch (error) {
   console.error(error);
 }
-
 
 
 //=====================================================
